a98d6dd89f4bd1bc85eae8f0898a4f4be87cec42
Mauri Mustonen <mauri.mustonen@tuni.fi>
2021-05-13 10:01:25 +0300

    llvm 13 support

--- lib/llvmopencl/WorkItemAliasAnalysis.cc
+++ lib/llvmopencl/WorkItemAliasAnalysis.cc
@@ -44,6 +44,12 @@ IGNORE_COMPILER_WARNING("-Wunused-parameter")
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 
+#ifdef LLVM_OLDER_THAN_13_0
+#define NO_ALIAS NoAlias
+#else
+#define NO_ALIAS AliasResult::Kind::NoAlias
+#endif
+
 POP_COMPILER_DIAGS
 
 using namespace llvm;
@@ -155,12 +161,12 @@ WorkItemAAResult::alias(const Location &LocA, const Location &LocB) {
     // pointer values are. This allows the code below to ignore this special
     // case.
     if (LocA.Size == 0 || LocB.Size == 0)
-        return NoAlias;
+        return NO_ALIAS;
 
     // Pointers from different address spaces do not alias
     if (cast<PointerType>(LocA.Ptr->getType())->getAddressSpace() != 
         cast<PointerType>(LocB.Ptr->getType())->getAddressSpace()) {
-        return NoAlias;
+        return NO_ALIAS;
     }
     // In case code is created by pocl, we can also use metadata.
     if (isa<Instruction>(LocA.Ptr) && isa<Instruction>(LocB.Ptr)) {
@@ -217,7 +223,7 @@ WorkItemAAResult::alias(const Location &LocA, const Location &LocB) {
                 if ( !(CIX->getValue() == CJX->getValue()
                     && CIY->getValue() == CJY->getValue()
                     && CIZ->getValue() == CJZ->getValue())) {
-                    return NoAlias;
+                    return NO_ALIAS;
                 }                
             }        
         }
